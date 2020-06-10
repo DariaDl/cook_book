d31f9d103851a390eb9e81d1f6eb4844542f94c6
Daria <D89110030406@gmail.com>
2020-06-10 22:21:32 +0300

    Update 2.0

--- cookbook.py
+++ cookbook.py
@@ -1,7 +1,7 @@
 def get_ing(i_line):
     i_line = i_line.split(' | ')
 
-    ing_dict = {"ingridient_name" : i_line[0] , 'quantity': i_line[1] ,"measure" : i_line[2].strip()}
+    ing_dict = {"ingridient_name" : i_line[0] , 'quantity': int(i_line[1]) ,"measure" : i_line[2].strip()}
     return ing_dict
 
 
@@ -16,21 +16,37 @@ def creating_dict(file_name):
                 ing_dict[x].append(get_ing(line))
     return ing_dict
 
-def get_shop_list_by_dishes(dishes, person_count,ing_dict):
-    list_by_dishes = { }
+
+
+
+def creating_dict(file_name):
+    ing_dict = {}
+    with open(file_name, 'r') as f:
+        for line in f:
+            if ('|' not in line) and (len(line) > 2):
+                x = line.strip()
+            elif '|' in line:
+                ing_dict.setdefault(x, [])
+                ing_dict[x].append(get_ing(line))
+    return ing_dict
+
+def get_shop_list_by_dishes(dishes, person_count ,ing_dict ):
+    list_by_dishes = {}
     for dish in dishes:
         for ingridient in ing_dict[dish]:
             if ingridient["ingridient_name"] not in list_by_dishes.keys():
-                list_by_dishes[ingridient["ingridient_name"]] = {"measure": ingridient["measure"], "quantity": ingridient["quantity"] * person_count}
+                list_by_dishes[ingridient["ingridient_name"]] = {"measure": ingridient["measure"],
+                                                                 "quantity": ingridient["quantity"] * person_count}
             else:
-                list_by_dishes["quantity"]  * person_count
+                list_by_dishes["quantity"] = ingridient["quantity"] * person_count
     print(list_by_dishes)
+
+
 
 print('Словарь из продуктов: ')
 
 print(creating_dict('df'))
 print()
 
-get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2, creating_dict('df'))
-
-
+print(get_shop_list_by_dishes(('Запеченный картофель', 'Омлет'), 2, creating_dict('df'))
+)
