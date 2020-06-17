2a4a280d192034bcfacc71cdaea5a78b38f44628
Daria <D89110030406@gmail.com>
2020-06-18 00:28:08 +0300

    new update

--- cookbook.py
+++ cookbook.py
@@ -1,3 +1,4 @@
+
 def get_ing(i_line):
     i_line = i_line.split(' | ')
 
@@ -19,28 +20,20 @@ def creating_dict(file_name):
 
 
 
-def creating_dict(file_name):
-    ing_dict = {}
-    with open(file_name, 'r') as f:
-        for line in f:
-            if ('|' not in line) and (len(line) > 2):
-                x = line.strip()
-            elif '|' in line:
-                ing_dict.setdefault(x, [])
-                ing_dict[x].append(get_ing(line))
-    return ing_dict
-
-def get_shop_list_by_dishes(dishes, person_count ,ing_dict ):
+def get_shop_list_by_dishes(*dishes,ingr_dict= creating_dict('df'), person_count = 6):
     list_by_dishes = {}
+    for key in ingr_dict.keys():
+        for dish in dishes:
+            if key == dish:
+                for dictionary in ingr_dict[key]:
+                    list_by_dishes_name = dictionary['ingridient_name']
+                    try:
+                        list_by_dishes[list_by_dishes_name]['quantity'] = (dictionary['quantity']*person_count)
+                    except:
+                           list_by_dishes[list_by_dishes_name] = {"measure": dictionary['measure'],  "quantity": dictionary['quantity'] * person_count}
     for dish in dishes:
-        for ingridient in ing_dict[dish]:
-            if ingridient["ingridient_name"] not in list_by_dishes.keys():
-                list_by_dishes[ingridient["ingridient_name"]] = {"measure": ingridient["measure"],
-                                                                 "quantity": ingridient["quantity"] * person_count}
-            else:
-                list_by_dishes["quantity"] = ingridient["quantity"] * person_count
-    print(list_by_dishes)
-
+        for element in list_by_dishes:
+            print(f"{element} = {list_by_dishes[element]}")
 
 
 print('Словарь из продуктов: ')
@@ -48,5 +41,4 @@ print('Словарь из продуктов: ')
 print(creating_dict('df'))
 print()
 
-print(get_shop_list_by_dishes(('Запеченный картофель', 'Омлет'), 2, creating_dict('df'))
-)
+get_shop_list_by_dishes(*['Омлет','Омлет'])
